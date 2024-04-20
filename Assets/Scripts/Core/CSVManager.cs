8e04ad997aff3e3c6f57f0a49d3e232957d37eb4
Kun <buptforeverbean@gmail.com>
2024-04-19 18:07:55 -0700

    update

--- Assets/Scripts/Core/CSVManager.cs
+++ Assets/Scripts/Core/CSVManager.cs
@@ -7,7 +7,21 @@ using System;
 namespace com.hive.projectr
 {
     #region CSV Data
-    public struct CSVCalibrationData
+    public struct CSVAsteroid
+    {
+        public int asteroidId;
+        public float spawnTime;
+        public Vector2 asteroidCoordinateWhenSpawned;
+        public Vector2 cursorCoordinateWhenAsteroidSpawned;
+        public float captureSec;
+        public Vector2 asteroidCoordinateWhenCaptured;
+        public Vector2 cursorCoordinateWhenAsteroidCaptured;
+        public bool isAsteroidCaptured;
+        public bool isAsteroidDestroyed;
+        public Vector2 vacuumCenterCoordinate;
+    }
+
+    public struct CSVCalibration
     {
         public Vector2 center;
         public Vector2 topLeft;
@@ -15,7 +29,7 @@ namespace com.hive.projectr
         public Vector2 bottomRight;
         public Vector2 bottomLeft;
 
-        public CSVCalibrationData(Vector2 center, Vector2 topLeft, Vector2 topRight, Vector2 bottomRight, Vector2 bottomLeft)
+        public CSVCalibration(Vector2 center, Vector2 topLeft, Vector2 topRight, Vector2 bottomRight, Vector2 bottomLeft)
         {
             this.center = center;
             this.topLeft = topLeft;
@@ -28,6 +42,16 @@ namespace com.hive.projectr
 
     public class CSVManager : SingletonBase<CSVManager>, ICoreManager
     {
+        private int _calibrationNum;
+        private int _coreGameNum;
+        private int _sessionNum;
+
+        private static readonly string DayFolderTemplate = "{0:00}/{1:00}/{2:0000}.{3}"; // 03/13/2024.Mark
+        private static readonly string SessionFolderTemplate = SessionNumPrefix + "{0}.{1:00}:{2:00}"; // Session#3.14:25
+        private static readonly string SessionNumPrefix = "Session#";
+        private static readonly string CoordinatePosFileTemplate = "{0}_{1}/{2}/{3}_{4}Block_Level{5}_CoordinatePOS"; // Mark_03/13/2024_100Block_Level1_CoordinatePOS
+        private static readonly string CoordinatesFileTemplate = "{0}_{1}/{2}/{3}_{4}Block_Level{5}_Coordinates"; // Mark_03/13/2024_100Block_Level1_Coordinates
+        private static readonly string SummaryFileTemplate = "{0}_Summary"; // Mark_Summary
 
 
 
@@ -42,7 +66,6 @@ namespace com.hive.projectr
 
         private bool _isRecording;
         private float _nextWriteTime;
-        private int _sessionNum;
 
         #region Lifecycle
         public void OnInit()
@@ -58,48 +81,155 @@ namespace com.hive.projectr
         }
         #endregion
 
-        #region Private
-        private void Setup()
-        {
-            var time = TimeUtil.Now;
-            var dayFolderPath = "";
-
-            try
-            {
-                var dayString = $"{time.ToString("yyyy-MM-dd")}.";
-
-#if UNITY_EDITOR
-                dayFolderPath = Path.Combine(Application.dataPath, "CSVFiles", $"{dayString}.{SettingManager.Instance.DisplayName}");
-#else
-                dayFolderPath = Path.Combine(Application.persistentDataPath, "CSVFiles", $"{dayString}.{SettingManager.Instance.DisplayName}");
-#endif
-
-                if (!Directory.Exists(dayFolderPath))
-                {
-                    Directory.CreateDirectory(dayFolderPath);
-                }
-
-            }
-            catch (Exception e)
-            {
-                Logger.LogException(e);
-            }
-        }
-        #endregion
-
-        #region Public
-
-
-
-        public void RecordCalibrationData(CSVCalibrationData data)
+        #region Calibration
+        public void OnCalibrationEnded(CSVCalibration data)
         {
             var dayFolderPath = SetupDayFolder(TimeUtil.Now);
 
             _csvWriter = new StreamWriter(_filePath, false);
-            _filePath = dayFolderPath + $"/movement_data_{dateTimeString}.csv";
+        }
+        #endregion
+
+        #region Core Game
+        public void OnAsteroidSpawned()
+        {
 
         }
+        #endregion
 
+        #region Private
+        private string GetDayFolderName(DateTime time, string username)
+        {
+            return string.Format(DayFolderTemplate, time.Month, time.Day, time.Year, username);
+        }
+
+        private string GetDayFolderPath(DateTime time, string username)
+        {
+#if UNITY_EDITOR
+            var dayFolderPath = Path.Combine(Application.dataPath, "CSVFiles", GetDayFolderName(time, username));
+#else
+            var dayFolderPath = Path.Combine(Application.persistentDataPath, "CSVFiles", GetDayFolderName(time, username));
+#endif
+            return dayFolderPath;
+        }
+
+        private string GetSessionFolderName(int sessionNum, DateTime time)
+        {
+            return string.Format(SessionFolderTemplate, sessionNum, time.Hour, time.Minute);
+        }
+
+        private string GetSessionFolderPath(string dayFolderPath, int sessionNum, DateTime time)
+        {
+            return Path.Combine(dayFolderPath, GetSessionFolderName(sessionNum, time));
+        }
+
+        private string GetCoordinatePosFileName(DateTime time, string username, int block, int level)
+        {
+            return string.Format(CoordinatePosFileTemplate, username, time.Month, time.Day, time.Year, block, level);
+        }
+
+        private string GetCoordinatePosFilePath(string sessionFolderPath, DateTime time, string username, int block, int level)
+        {
+            return Path.Combine(sessionFolderPath, $"{GetCoordinatePosFileName(time, username, block, level)}.csv");
+        }
+
+        private string GetCoordinatesFileName(DateTime time, string username, int block, int level)
+        {
+            return string.Format(CoordinatesFileTemplate, username, time.Month, time.Day, time.Year, block, level);
+        }
+
+        private string GetCoordinatesFilePath(string sessionFolderPath, DateTime time, string username, int block, int level)
+        {
+            return Path.Combine(sessionFolderPath, $"{GetCoordinatesFileName(time, username, block, level)}.csv");
+        }
+
+        private string GetSummaryFileName(string username)
+        {
+            return string.Format(SummaryFileTemplate, username);
+        }
+
+        private string GetSummaryFilePath(string sessionFolderPath, string username)
+        {
+            return Path.Combine(sessionFolderPath, $"{GetSummaryFileName(username)}.csv");
+        }
+
+        private bool TryParseSessionFolder(string sessionFolderName, out int sessionNum)
+        {
+            sessionNum = 1;
+
+            var sessionStr = sessionFolderName.Substring(sessionFolderName.IndexOf(SessionNumPrefix) + SessionNumPrefix.Length);
+            if (sessionStr.Length > 0)
+            {
+                var sessionNumStr = sessionStr.Substring(0, sessionStr.IndexOf('.'));
+                if (int.TryParse(sessionNumStr, out sessionNum) && sessionNum >= 1)
+                {
+                    return true;
+                }
+            }
+
+            Debug.LogError($"Invalid session folder name detected! Please check! -> {sessionFolderName}");
+            return false;
+        }
+
+        /// <summary>
+        /// Need to be called before writing any log to local file
+        /// </summary>
+        /// <returns>If success</returns>
+        private bool Setup()
+        {
+            var time = TimeUtil.Now;
+
+            try
+            {
+                var dayFolderPath = GetDayFolderPath(time, SettingManager.Instance.DisplayName);
+
+                if (!Directory.Exists(dayFolderPath))
+                {
+                    Directory.CreateDirectory(dayFolderPath);
+                    _sessionNum = 0;
+                }
+
+                // check existing session folders within day folder
+                var sessionNum = 0;
+                var sessionFolders = Directory.GetDirectories(dayFolderPath);
+                foreach (var sessionFolder in sessionFolders)
+                {
+                    if (TryParseSessionFolder(sessionFolder, out var pSessionNum))
+                    {
+                        sessionNum = Mathf.Max(sessionNum, pSessionNum);
+                    }
+                }
+                sessionNum = Mathf.Max(sessionNum, 0);
+
+                if (_sessionNum == 0 || _sessionNum != sessionNum) // init session (folder and stuff)
+                {
+                    _sessionNum = sessionNum + 1;
+                }
+
+                var sessionFolderPath = GetSessionFolderPath(dayFolderPath, _sessionNum, time);
+                if (!Directory.Exists(sessionFolderPath))
+                {
+                    Directory.CreateDirectory(sessionFolderPath);
+                }
+
+                /*
+                var coordinatePosFilePath = GetCoordinatePosFilePath(sessionFolderPath, time, SettingManager.Instance.DisplayName, SettingManager.Instance.DailyBlock, SettingManager.Instance.Level);
+                var coordinatesFilePath = GetCoordinatesFilePath(sessionFolderPath, time, SettingManager.Instance.DisplayName, SettingManager.Instance.DailyBlock, SettingManager.Instance.Level);
+                var summaryFilePath = GetSummaryFilePath(sessionFolderPath, SettingManager.Instance.DisplayName);
+                */
+            }
+            catch (Exception e)
+            {
+                Logger.LogException(e);
+                return false;
+            }
+
+            Logger.Log($"CSVManager::Setup - _sessionNum: {_sessionNum} | _calibrationNum: {_calibrationNum} | _coreGameNum: {_coreGameNum}");
+            return true;
+        }
+        #endregion
+
+        #region Public
         public void StartRecording(Transform handObj)
         {
             _handObj = handObj;
